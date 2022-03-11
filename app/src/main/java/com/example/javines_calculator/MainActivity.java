99fa382da56af029f323569c8c96a03e3b4e305c
justine javines <97206960+Crayzjbs@users.noreply.github.com>
2022-03-11 15:43:09 +0800

    method test

--- app/src/main/java/com/example/javines_calculator/MainActivity.java
+++ app/src/main/java/com/example/javines_calculator/MainActivity.java
@@ -14,11 +14,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //To be called later on
     //to be used variables
 
-    EditText txtVar1,txtVar2;
-    Button btnAdd, btnSub, btnMulti, btnDivide, btnModulo;
-    TextView txtAns;
+    EditText txtVariable1,txtVariable2;
+    Button btnAdd, btnSubtract, btnMultiply, btnDivide, btnModulo;
+    TextView txtAnswer;
 
-    double var1, var2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,63 +28,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //calling process
         //declarations
 
-        btnAdd = findViewById(R.id.btnAdd);
-        btnSub = findViewById(R.id.btnSub);
-        btnMulti = findViewById(R.id.btnMulti);
-        btnDivide = findViewById(R.id.btnDivide);
-        btnModulo = findViewById(R.id.btnModulo);
-
-        //Answer text view
-        txtAns = findViewById(R.id.txtAnswer);
+        Button btnAdd = findViewById(R.id.btnAdd);
+        Button btnSubtract = findViewById(R.id.btnSubtract);
+        Button btnMultiply = findViewById(R.id.btnMultiply);
+        Button btnDivide = findViewById(R.id.btnDivide);
+        Button btnModulo = findViewById(R.id.btnModulo);
 
         //to make the button work (listener)
-
         btnAdd.setOnClickListener(this);
-        btnSub.setOnClickListener(this);
-        btnMulti.setOnClickListener(this);
+        btnSubtract.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
         btnDivide.setOnClickListener(this);
         btnModulo.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        //variable text boxes
-        txtVar1 = findViewById(R.id.txtVariable1);
-        txtVar2 = findViewById(R.id.txtVariable2);
 
-        var1 = Double.parseDouble(txtVar1.getText().toString());
-        var2 = Double.parseDouble(txtVar2.getText().toString());
+        double txtVariable1=0;
+        double txtVariable2=0;
+        double txtAnswer=0;
+
+        //variable text boxes
+        EditText txtVar1 = findViewById(R.id.txtVariable1);
+        EditText txtVar2 = findViewById(R.id.txtVariable2);
+        TextView txtAns = findViewById(R.id.txtAnswer);
+
+        txtVariable1= Double.parseDouble(txtVar1.getText().toString());
+        txtVariable2 = Double.parseDouble(txtVar2.getText().toString());
 
 
         switch(v.getId()){
             case R.id.btnAdd:
-                txtAns.setText(Double.toString(var1 + var2));
-
+                txtAns.setText(add(txtVariable1, txtVariable2));
                 break;
-            case R.id.btnSub:
-                txtAns.setText(Double.toString(var1 - var2));
-
+            case R.id.btnSubtract:
+                txtAns.setText(subtract(txtVariable1, txtVariable2));
                 break;
-            case R.id.btnMulti:
-                txtAns.setText(Double.toString(var1 * var2));
-
+            case R.id.btnMultiply:
+                txtAns.setText(multiply(txtVariable1, txtVariable2));
                 break;
             case R.id.btnDivide:
-                txtAns.setText(Double.toString(var1 / var2));
-
+                txtAns.setText(divide(txtVariable1, txtVariable2));
                 break;
             case R.id.btnModulo:
-                txtAns.setText(Double.toString(var1 % var2));
-
+                txtAns.setText(modulo(txtVariable1, txtVariable2));
                 break;
 
         }
 
     }
     //Methods
-
-
-
-
+    String add(double var1, double var2){return String.valueOf(var1 + var2);}
+    String subtract(double var1, double var2){return String.valueOf(var1 - var2);}
+    String multiply(double var1, double var2){return String.valueOf(var1 * var2);}
+    String divide(double var1, double var2){return String.valueOf(var1 / var2);}
+    String modulo(double var1, double var2){return String.valueOf(var1 % var2);}
 
 }
